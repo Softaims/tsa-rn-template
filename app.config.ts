491bc973be01f7948f4207c4799af1be63e474bd
sohaib-tsa <sohaib@scaleupsoftware.io>
2026-01-19 16:38:18 +0500

    fix: workflows update

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 import { ConfigContext, ExpoConfig } from "expo/config";
 import { name, version } from "./package.json";
-// 1
+// 2
 type AppEnvironment = "development" | "preview" | "production";
 
 /**
